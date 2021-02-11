401e57bfebfdabdaa140a3512fc045f4eb075701
Jeffrey Lue <jeffrey.lue23@ycdsbk12.ca>
2021-02-11 15:00:31 +0000

    '

--- minutes_days.py
+++ minutes_days.py
@@ -1,4 +1,7 @@
 number_of_minutes = int(input("Enter the number of minutes: "))
-number_of_hours = number_of_minutes/60
-number_of_days = number_of_hours/24
+number_of_days = number_of_minutes//1440
+remaining_minutes = minutes%1440
+hours = remaining_minutes//60
+final_minutes = remaining_minutes - hours*60
+
 print ("The number of days is " + str(number_of_days) + " the number of hours left is " + str(number_of_hours) + " the number of minutes are" + str(number_of_minutes))
--- windchill.py
+++ windchill.py
@@ -1,2 +1,9 @@
 temperature = float(input("Enter the temperature in celcius: "))
-windspeed
+windspeed = float(input("Enter the windspeed (km/h): "))
+
+# compute windchill
+windchill = 13.12 + (0.6215*temperature) - (11.37 * windspeed**0.16) + (0.3965 * temperature* windspeed**0.16)
+
+# output windchill
+print("With the windchill factor, it feels like " + str(round(windchill,1)) + "° outside.")
+
